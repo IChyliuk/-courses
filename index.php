cfbf9fec51bd93b3e9746abfce905ebb45daa86b
IChyliuk <cilukivan@gmail.com>
2024-06-03 22:43:09 +0300

    final

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-$array = array(0, -1, -5, 3, 52, 101, 364, 1);
+$array = array(0, -1, -5, 3, 52, 101, 364, 1); // массив
 var_dump($array);
-    for ($i = 0; $i < sizeof($array); $i++) {
+    for ($i = 0; $i < sizeof($array); $i++) { // сортировка
         for ($j = 0; $j < sizeof($array) - 1 - $i; $j++) {
             if ($array[$j] > $array[$j + 1]) {
                 $temp_value = $array[$j];
@@ -10,4 +10,4 @@ var_dump($array);
             }
         }
     }
-    var_dump($array);
+    var_dump($array); // вывод
