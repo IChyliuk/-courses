b84797a0159a9b81ad215f161620c2c5f6fc0cf2
IChyliuk <cilukivan@gmail.com>
2024-06-03 22:26:15 +0300

    bubble_sort

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$array = array(0, -1, -5, 3, 52, 101, 364, 1);
+var_dump($array);
+    for ($i = 0; $i < sizeof($array); $i++) {
+        for ($j = 0; $j < sizeof($array) - 1 - $i; $j++) {
+            if ($array[$j] > $array[$j + 1]) {
+                $temp_value = $array[$j];
+                $array[$j] = $array[$j + 1];
+                $array[$j + 1] = $temp_value;
+            }
+        }
+    }
+    var_dump($array);
