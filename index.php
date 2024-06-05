81ff321747283331273a144cb4c402d3fb6b859a
IChyliuk <cilukivan@gmail.com>
2024-06-05 22:50:12 +0300

    update bubble sort

--- index.php
+++ index.php
@@ -1,14 +1,27 @@
 <?php
-$array = array(0, -1, -5, 3, 52, 101, 364, 1); // массив элементов
-$array = array(0, -1, -5, 3, 52, 101, 364, 1); // массив
+$array = array(0, -1, 1, 2, 3, 4, 5, 6); // массив элементов
 var_dump($array);
-for ($i = 0; $i < sizeof($array); $i++) { // сортировка
-    for ($j = 0; $j < sizeof($array) - 1 - $i; $j++) {
-        if ($array[$j] > $array[$j + 1]) {
-            $temp_value = $array[$j];
-            $array[$j] = $array[$j + 1];
-            $array[$j + 1] = $temp_value;
+bubble_sort($array, $time);
+function bubble_sort(&$array, &$time): void
+{
+    $start = microtime(true);
+    $i = 0;
+    $check = true;
+    $count = count($array);
+    while ($check && $i < $count) {
+        $check = false;
+        for ($j = 0; $j < $count - 1 - $i; $j++) {
+            if ($array[$j] > $array[$j + 1]) {
+                $temp_value = $array[$j];
+                $array[$j] = $array[$j + 1];
+                $array[$j + 1] = $temp_value;
+                $check = true;
+            }
         }
+        $i++;
     }
+    $time = round(microtime(true) - $start, 10);
 }
-var_dump($array); // вывод x
+
+var_dump($array) . PHP_EOL;
+echo 'Время выполнения - ' . $time . PHP_EOL;
