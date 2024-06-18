2cf145ae518258112846d4021f09b2cbcaec02eb
IChyliuk <cilukivan@gmail.com>
2024-06-18 17:52:05 +0300

    recursion func

---
+++ recursion/index.php
@@ -0,0 +1,17 @@
+<?php
+function recursion($segments_array, $count): array
+{
+    if ($count >= count($segments_array) - 1) {
+        return $segments_array;
+    }
+    $check = $segments_array[$count][1] >= $segments_array[$count+1][0];
+    if ($check) {
+        $segments_array[$count][1] = $segments_array[$count+1][1];
+        array_splice($segments_array, $count+1, 1);
+       return recursion($segments_array, $count);
+    }
+    else{
+        $count++;
+        return recursion($segments_array, $count);
+    }
+}
